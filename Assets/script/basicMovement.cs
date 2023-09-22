8cf48b364712affd14223623c132a27410680241
Retro-Bro4 <jayrod9184@gmail.com>
2023-09-21 20:09:59 -0400

    added reset and sound

--- Assets/script/basicMovement.cs
+++ Assets/script/basicMovement.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class basicMovement : MonoBehaviour
 {
+    public GameObject hazard;
+    public GameObject[] hazards;
+   // public GameObject[] coins;
+    Vector3 startPos;
+    public AudioClip deathlul;
+    AudioSource player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        player = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W)){
+            transform.position += Vector3.forward;
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            transform.position += Vector3.back;
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            transform.position += Vector3.right;
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+            transform.position += Vector3.left;   
+        }/*
+        if(Input.GetKeyDown(KeyCode.Q)){
+            transform.position += Vector3.down;   
+        }
+        if(Input.GetKeyDown(KeyCode.E)){
+            transform.position += Vector3.up;   
+        }*/
+/*
+        if(transform.position == hazard.transform.position){
+            Debug.Log("you are hazards :<");
+            Destroy(gameObject);
+        }
+*/
+        for(int i=0; 1 < hazards.Length; i++){
+            if(transform.position == hazards[i].transform.position){
+            Debug.Log("you are hazards :<");
+            //Destroy(gameObject);
+            transform.position = startPos;
+            player.PlayOneShot(deathlul, 0.75f);
+        }
+/*
+        }
+        for(int i=0; 1 < coins.Length; i++){
+            if(transform.position == coins[i].transform.position){
+            Destroy(coins);
+            }*/
+
+        }
     }
 }
